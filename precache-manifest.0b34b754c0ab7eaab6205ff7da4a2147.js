69cb7ebdf42f54365c04b9985c237a8709319cc7
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-23 19:16:58 +0200

    Updates

---
+++ precache-manifest.0b34b754c0ab7eaab6205ff7da4a2147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8561eca1459cc3e8f4ba8402fe8643",
+    "url": "./index.html"
+  },
+  {
+    "revision": "897e57c97f2ae12943ee",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "7f36aef92ff757f8b234",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "897e57c97f2ae12943ee",
+    "url": "./static/js/2.ea1416f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ea1416f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f36aef92ff757f8b234",
+    "url": "./static/js/main.5c810134.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
